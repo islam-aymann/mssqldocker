59f1c86d245a34d574a61ba66c0cb48022c5e5e0
Islam Ayman <e-i.mohammed@lean.sa>
2021-04-22 05:06:06 +0200

    replace database user

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-###### MSSQL Docker Image
+# MSSQL Docker Image
+
+###### Required ENV variables
+      ACCEPT_EULA: Y
+      MSSQL_PID: Express
+
+      SA_PASSWORD: "yourStrong(!)Password"
+
+      MSSQL_USER: mssqldocker
+      MSSQL_DB: mssqldocker
+      MSSQL_PASSWORD: "yourStrong(!)PasswordForMssqldocker"
+
+
--- run-initialization.sh
+++ run-initialization.sh
@@ -1,5 +1,42 @@
-# Wait to be sure that SQL Server came up
-sleep 10s
+#!/bin/sh
+# wait for MSSQL server to start
+sleep 6s
 
-# Run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d master -Q "CREATE DATABASE [$DB_NAME]"
+export STATUS=1
+i=0
+max_time=300
+
+while [ $STATUS -ne 0 ] && [ $i -lt $max_time ]; do
+  i=$((i + 1))
+  echo "Trying to connect to localhost (#$i) using user: sa."
+  /opt/mssql-tools/bin/sqlcmd -t 1 -S localhost -U sa -P "$SA_PASSWORD" -Q "select 1" >>/dev/null
+  STATUS=$?
+  sleep 1
+done
+
+if [ $STATUS -ne 0 ]; then
+  echo "Error: MSSQL SERVER took more than $max_time seconds to start up."
+  exit 1
+fi
+
+echo "MSSQL Server is ready. $i seconds."
+
+echo "Trying to create $MSSQL_DB database using sa user."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d master -Q "CREATE DATABASE [$MSSQL_DB]"
+
+echo "Trying to create login for: $MSSQL_USER."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d "$MSSQL_DB" \
+  -Q "CREATE LOGIN [$MSSQL_USER] WITH PASSWORD = '$MSSQL_PASSWORD', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF"
+
+echo "Trying to create user for: $MSSQL_USER."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d "$MSSQL_DB" \
+  -Q "CREATE USER [$MSSQL_USER] FOR LOGIN [$MSSQL_USER]"
+
+echo "Trying to grant permissions to: $MSSQL_USER."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d "$MSSQL_DB" \
+  -Q "EXEC sp_addrolemember 'db_owner', $MSSQL_USER "
+
+
+echo "MSSQL Server is ready..."
+
+exec "$@"
