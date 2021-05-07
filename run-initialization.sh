2ba9e5a709706cbc15b72a61450b268175a06e1b
Islam Ayman <e-i.mohammed@lean.sa>
2021-05-07 03:50:59 +0200

    Alter usage of SQL lines into one file

--- run-initialization.sh
+++ run-initialization.sh
@@ -21,22 +21,8 @@ fi
 
 echo "MSSQL Server is up. $i seconds."
 
-echo "Trying to create $MSSQL_DB database using sa user."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d master -Q "CREATE DATABASE [$MSSQL_DB]"
-
-echo "Trying to create login for: $MSSQL_USER."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d "$MSSQL_DB" \
-  -Q "CREATE LOGIN [$MSSQL_USER] WITH PASSWORD = '$MSSQL_PASSWORD', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF"
-
-echo "Trying to create user for: $MSSQL_USER."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d "$MSSQL_DB" \
-  -Q "CREATE USER [$MSSQL_USER] FOR LOGIN [$MSSQL_USER]"
-
-echo "Trying to grant permissions to: $MSSQL_USER."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d "$MSSQL_DB" \
-  -Q "EXEC sp_addrolemember 'db_owner', $MSSQL_USER "
-
-
+echo "MSSQL configurations started..."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d master -i setup.sql
 echo "MSSQL Server is ready..."
 
 exec "$@"
